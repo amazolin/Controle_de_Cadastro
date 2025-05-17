d49f7363dd8903e62847e46205c551bd5b23b121
RaphaelGNunhez <raphaelgodeknunhez@gmail.com>
2025-05-16 21:27:55 -0300

    Tela de Cadastro

--- app/src/main/java/com/example/controle_de_cadastro/Cadastro.java
+++ app/src/main/java/com/example/controle_de_cadastro/Cadastro.java
@@ -3,6 +3,8 @@ package com.example.controle_de_cadastro;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -11,43 +13,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Cadastro extends AppCompatActivity {
 
-    Button btnLogin , btnCadastrar;
+    Button btnCadastrar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cadastro);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        btnLogin = findViewById(R.id.btnLogin);
         btnCadastrar = findViewById(R.id.btnCadastrar);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent it = new Intent(getApplicationContext(), menuusuario.class);
-                startActivity(it);
-            }
-        });
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent in = new Intent(getApplicationContext(), Cadastro.class);
+                Intent in = new Intent(Cadastro.this, MainActivity.class);
                 startActivity(in);
-                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
-
-
     }
 }
